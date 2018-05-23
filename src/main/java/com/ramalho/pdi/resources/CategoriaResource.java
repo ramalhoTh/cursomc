32c85a29a86b224c3a8e082575393d113b8e3356
Thiago Ramalho <t.oliveira.ramalho@gmail.com>
2018-05-23 09:22:35 -0300

    Testando a primeira classe de dominio - Categoria

--- src/main/java/com/ramalho/pdi/resources/CategoriaResource.java
+++ src/main/java/com/ramalho/pdi/resources/CategoriaResource.java
@@ -1,16 +1,32 @@
 package com.ramalho.pdi.resources;
 
+
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ramalho.pdi.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar()
+	public List<Categoria> listar()
 	{
-		return "Esta Funcionado";
+		Categoria  cat1 = new Categoria(1, "Informatica");
+		Categoria  cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		
+		return lista;
 	}
 }
